dbfb4ca2c047813cf31c76c29085e1215b93dc05
leTabs <125308756+leTabs@users.noreply.github.com>
2023-07-11 14:10:55 +0300

    Update blogFeed.js

--- Blog-Webpage-Template/Backend- Utility/blogFeed.js
+++ Blog-Webpage-Template/Backend- Utility/blogFeed.js
@@ -1,10 +1,25 @@
 
-
 const blogThumb = document.querySelector('.main-blog-section')
 blogThumb.innerHTML= `
 `
 const newBlog = function(
-
-  
+    imageThumb='pexels-adam-lowly-2622290.jpg', 
+    date='Month 00', blogTitle='Title',
+    blogDescription='A small and meaningful descriptionA small and meaningful descriptionA small and meaningful descriptionA small and meaningful descriptionA small and meaningful description '
+    ){
+    
+    blogThumb.innerHTML += `
+            <section class="blog-thumb">
+            <img src="images/${imageThumb}" alt="" class="blog-thumb-img"/>
+            <span>
+                <p>${date}</span> <img src="icons/ellipsis-vertical-solid.svg" alt="" class="vertical-elip"/></p>
+                <h3 class="blog-thumb-title">${blogTitle}</h3>
+                <p>${blogDescription}</p>
+            </span>
+        </section>
+    `
 }
+newBlog()
+newBlog()
+newBlog()
 newBlog()
