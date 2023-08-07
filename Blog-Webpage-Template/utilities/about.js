e5a94d2d38051a9c8e3dd2595ab8f67e2745c362
leTabs <125308756+leTabs@users.noreply.github.com>
2023-08-08 00:06:06 +0300

    Update about.js

--- Blog-Webpage-Template/utilities/about.js
+++ Blog-Webpage-Template/utilities/about.js
@@ -1,25 +1,25 @@
 // ANCHOR VARIABLE VALUES (required)
 
 // title
-const aboutTitle = 'Get to me better'
+const aboutTitle = 'About Section Title'
 // the text content (paragraph)
 const paragraph = `
-    I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+Text content that describes valuable information about the author, ownser or manager of the website. Text content that describes valuable information about the author, ownser or manager of the website. Text content that describes valuable information about the author, ownser or manager of the website. 
     <br /> <br />
-    This is a great space to write a long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.
+    Text content that describes valuable information about the author, ownser or manager of the website. Text content that describes valuable information about the author, ownser or manager of the website. Text content that describes valuable information about the author, ownser or manager of the website. 
 `
 // the image's name 
-const aboutImageName = 'pexels-szabolcs-toth-10182592.jpg'
+const aboutImageName = 'proxy03.png'
 // the image's alternative text
-const aboutImgAltText = 'this is an image'
+const aboutImgAltText = 'proxy image could not load'
 
-// anchor variable values required END
+/////////////////////////////////////////
+// VARIABLES (not required)
+
+// getting the empty element:
+const mainAbout = document.querySelector('.main-about-section')
 /*---------------------------------------------------------------------------------------------*/
 // FUNCTION DEFINITION
-
-
-// getting the empty element (type: main, className: "main-about-section")
-const mainAbout = document.querySelector('.main-about-section')
 
 // the function defines the structure of the empty element and specifies it's content 
 // from the variables, passed as arguments during the function call
@@ -35,7 +35,6 @@ const aboutContent = function(title, text, image, altText){
     `
 }
 
-// function definition END
 /*---------------------------------------------------------------------------------------------*/
 // FUNCTION CALL
 
