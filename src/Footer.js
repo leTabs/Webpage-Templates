49d63d0b8e747000086c24462a2c6aaf681d635c
leTabs <125308756+leTabs@users.noreply.github.com>
2023-10-18 23:53:27 +0300

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer className="Footer-section">
+            <span>
+                &copy; 2023 by Granger & Anderson. Powered by <a href="">leTabs</a>.
+            </span>
+        </footer>
+    )
+}
+export default Footer
