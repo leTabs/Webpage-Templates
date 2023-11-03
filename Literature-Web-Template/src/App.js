2525f46138a9072e580cf620fdab90acdabb8e49
leTabs <125308756+leTabs@users.noreply.github.com>
2023-11-03 21:38:19 +0200

    Create App.js

---
+++ Literature-Web-Template/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Navigation';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation></Navigation>
+    </div>
+  );
+}
+
+export default App;
