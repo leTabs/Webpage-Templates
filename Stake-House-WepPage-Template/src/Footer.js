ca1686dd63bb3481833342f4f335e0f545f310f2
leTabs <125308756+leTabs@users.noreply.github.com>
2023-11-08 11:44:28 +0200

    Create Footer.js

---
+++ Stake-House-WepPage-Template/src/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <footer className="Footer">
+            <span>
+                <p>&copy; by the Restayrant.</p>
+                <p>Powered by <a href="">leTabs</a></p>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer;
