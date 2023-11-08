a1abbc131a1fe5681acaa723ee09d78f0e22e4ba
leTabs <125308756+leTabs@users.noreply.github.com>
2023-11-08 11:44:02 +0200

    Update App.js

--- Stake-House-WepPage-Template/src/App.js
+++ Stake-House-WepPage-Template/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
 import Opening from './Opening';
+import About from './About';
+import Menu from './Menu';
+import Reservations from './Reservations';
+import RestaurantInfo from './RestaurantInfo';
+import Footer from './Footer';
 
 
 function App() {
@@ -10,8 +15,11 @@ function App() {
       <NavBar></NavBar>
       <div className="App-wrapper">
         <Opening></Opening>
-        <div className="pop">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, animi. Animi, tempora fugiat? Minima ratione beatae minus ad reiciendis sit, voluptas recusandae fugit laboriosam rem eos aspernatur dolores temporibus impedit iste neque blanditiis, officia dolorum. Aperiam voluptate ipsa voluptas animi, non natus molestias reiciendis fugiat corrupti cupiditate excepturi vel, corporis nulla perspiciatis nisi itaque eligendi reprehenderit dolorem fugit eaque dolor necessitatibus labore unde ipsam. Sunt maxime officia sequi? Cum dolorum laborum dicta, rem repellat quos. Fuga laboriosam a inventore id unde, asperiores odit accusamus, optio veritatis obcaecati est nihil facilis aut. Totam voluptatum rem voluptatibus culpa veniam ipsam in dolore nam tenetur laudantium exercitationem voluptates obcaecati hic mollitia cupiditate fugiat eligendi reiciendis officiis quibusdam ratione, minima dignissimos expedita fugit? Voluptatem quisquam corporis repudiandae aperiam. Quos enim dolorem repudiandae inventore odit porro accusamus sit ullam sint ut animi nemo velit, obcaecati minima! Eveniet assumenda eum neque dolores ratione ut incidunt accusantium, autem id a voluptatibus ullam quam aspernatur ex facilis ad fugit, et cum quaerat optio odit rem fuga aliquid deserunt! Eaque possimus, reprehenderit numquam pariatur sed impedit quibusdam excepturi itaque delectus magnam! Impedit aliquid numquam omnis sequi. Tenetur quod quaerat nam quam amet, enim, iure molestiae aspernatur maiores expedita nisi sint consequuntur ducimus officiis dolore quisquam provident minus! Adipisci possimus nam iste consequatur omnis architecto quasi voluptatum magnam velit in nihil excepturi tempora dolores, odit enim quos fuga porro ipsum nemo quam sit doloribus. Ex nobis mollitia possimus minus voluptates dicta voluptatem sit amet, odio voluptate tempora optio saepe eius quis, eligendi incidunt fugit non qui quam recusandae. Id beatae iusto ratione totam facere odio sunt animi saepe consequuntur! Nesciunt, esse veniam? Tempora ad cupiditate sunt saepe doloremque enim vel qui eligendi pariatur eveniet culpa corporis, odit animi nihil, consequuntur sequi ducimus non! Accusamus culpa beatae eum. Odit, mollitia error.
-        </div>
+        <About></About>
+        <Menu></Menu>
+        <Reservations></Reservations>
+        <RestaurantInfo></RestaurantInfo>
+      <Footer></Footer>
       </div>
     </div>
   );
