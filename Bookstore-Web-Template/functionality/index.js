efd6141a4aebb43700fe5e94e8b13d9b74b4790b
leTabs <125308756+leTabs@users.noreply.github.com>
2024-03-06 23:46:01 +0200

    Update index.js

--- Bookstore-Web-Template/functionality/index.js
+++ Bookstore-Web-Template/functionality/index.js
@@ -26,3 +26,43 @@ continueBtn.addEventListener('click', ()=>{
 backToTheTopButton.addEventListener('click', ()=>{
     sectionOpening.scrollIntoView();
 })
+
+
+
+
+//
+function animationScrollTrigger(target, animation){
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6
+};
+
+// Define the callback function
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(animation);
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(target)};
+
+const x = document.querySelector('.item-01')
+const y = document.querySelector('.item-02')
+const z = document.querySelector('.item-03')
+animationScrollTrigger(x, 'item-jumpers')
+animationScrollTrigger(y, 'item-jumpers')
+animationScrollTrigger(z, 'item-jumpers')
+
+const x2 = document.querySelector('.p-01')
+const y2 = document.querySelector('.p-02')
+const z2 = document.querySelector('.p-03')
+animationScrollTrigger(x2, 'text-info-animator')
+animationScrollTrigger(y2, 'text-info-animator')
+animationScrollTrigger(z2, 'text-info-animator')
