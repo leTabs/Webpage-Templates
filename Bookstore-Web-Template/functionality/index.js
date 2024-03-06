00c46a11294171d2cffd1e7f2697ef859e9d09cc
leTabs <125308756+leTabs@users.noreply.github.com>
2024-03-06 20:41:42 +0200

    Update index.js

--- Bookstore-Web-Template/functionality/index.js
+++ Bookstore-Web-Template/functionality/index.js
@@ -1,13 +1,28 @@
+
+
+
 const navDestinationsArray = document.querySelectorAll('.nav-destination')
 
 const sectionOpening = document.querySelector('.opening-section')
 const sectionAbout = document.querySelector('.about-section')
+const sectionProductsServices = document.querySelector('.products-and-services')
+const sectionContact = document.querySelector('.contact-section')
 
+const sectionsArray = [sectionOpening, sectionAbout, sectionProductsServices, sectionContact]
 
-// alert(navDestinationsArray.length)
-navDestinationsArray[0].addEventListener('click', ()=>{
-    sectionOpening.scrollIntoView()
+const continueBtn = document.querySelector('.continue-btn')
+const backToTheTopButton = document.querySelector('.back-to-the-top-btn')
+
+for(let i = 0; i < navDestinationsArray.length; i++){
+    navDestinationsArray[i].addEventListener('click', ()=>{
+        sectionsArray[i].scrollIntoView()
+    })
+}
+
+continueBtn.addEventListener('click', ()=>{
+    sectionProductsServices.scrollIntoView()
 })
-navDestinationsArray[1].addEventListener('click', ()=>{
-    sectionAbout.scrollIntoView()
+
+backToTheTopButton.addEventListener('click', ()=>{
+    sectionOpening.scrollIntoView();
 })
