ce65cc03c7b25d59d985135edd75f1691f05adb7
leTabs <125308756+leTabs@users.noreply.github.com>
2024-03-07 21:07:58 +0200

    Update index.js

--- Bookstore-Web-Template/functionality/index.js
+++ Bookstore-Web-Template/functionality/index.js
@@ -53,12 +53,19 @@ const observer = new IntersectionObserver(callback, options);
 
 observer.observe(target)};
 
-const x = document.querySelector('.item-01')
-const y = document.querySelector('.item-02')
-const z = document.querySelector('.item-03')
-animationScrollTrigger(x, 'item-jumpers')
-animationScrollTrigger(y, 'item-jumpers')
-animationScrollTrigger(z, 'item-jumpers')
+const item01 = document.querySelector('.item-01')
+const item02 = document.querySelector('.item-02')
+const item03 = document.querySelector('.item-03')
+
+
+// const item04 = document.querySelector('.item-04')
+// const item05 = document.querySelector('.item-05')
+// const item06 = document.querySelector('.item-06')
+
+
+animationScrollTrigger(item01, 'item-jumpers')
+animationScrollTrigger(item02, 'item-jumpers')
+animationScrollTrigger(item03, 'item-jumpers')
 
 const x2 = document.querySelector('.p-01')
 const y2 = document.querySelector('.p-02')
@@ -68,3 +75,46 @@ animationScrollTrigger(y2, 'text-info-animator')
 animationScrollTrigger(z2, 'text-info-animator')
 
 animationScrollTrigger(document.querySelector('.contact-img'), 'contact-img-animation')
+
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+
+
+const allItems = document.querySelectorAll('.item')
+const allDots = document.querySelectorAll('.dot')
+
+let itemCounter = 0;
+
+function nextItem(itemIndex){
+  if(itemCounter == allItems.length - 3){
+    return(alert('No more items'))
+  }
+    allItems[itemIndex].style.display = 'none'
+    allDots[itemIndex].style.backgroundColor = 'white';
+
+    allItems[itemIndex + 3].style.display = 'block'
+    allDots[itemIndex + 3].style.backgroundColor ='black'
+    itemCounter++
+    // alert(itemCounter)
+}
+
+
+function prevItem(itemIndex){
+  if(itemCounter == 0){
+    return
+  }
+  allItems[itemIndex - 1].style.display = 'block';
+  allDots[itemIndex - 1].style.backgroundColor ='black'
+
+  allItems[itemIndex + 2].style.display = 'none';
+  allDots[itemIndex + 2].style.backgroundColor = 'white';
+  itemCounter--
+}
+
+
+prevBtn.addEventListener('click', ()=>{prevItem(itemCounter)})
+nextBtn.addEventListener('click', ()=>{nextItem(itemCounter)})
+
+const aboutLeftDiv = document.querySelector('.about-left-div')
+animationScrollTrigger(aboutLeftDiv, 'about-left-div-appearing')
