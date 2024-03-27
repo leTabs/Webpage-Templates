757230d3f1c8ae08d5d7dc28a4db1a698d2d2fa5
leTabs <125308756+leTabs@users.noreply.github.com>
2024-03-27 22:23:17 +0200

    Update index.js

--- Yoga-Web-Template/functionality/index.js
+++ Yoga-Web-Template/functionality/index.js
@@ -1,7 +1,21 @@
-const navBar = document.querySelector('.nav-bar')
-const moveToExercisesBtn = document.querySelector('.move-to-exercises-btn')
+// sections
+const sectionHome = document.querySelector('.section-home')
+const sectionAbout = document.querySelector('.section-about')
 const sectionExercises = document.querySelector('.section-exercises')
+const sectionMyVision = document.querySelector('.section-my-vision')
+const sectionCommunication = document.querySelector('.section-communication')
 
+const navBar = document.querySelector('.nav-bar')
+const navHome = document.querySelector('.nav-home')
+const navAbout = document.querySelector('.nav-about')
+const navExercises = document.querySelector('.nav-exercises')
+const navMyVision = document.querySelector('.nav-my-vision')
+const navCommunication = document.querySelector('.nav-communication')
+const moveToExercisesBtn = document.querySelector('.move-to-exercises-btn')
+
+
+const navAnchorsArray = [navHome, navAbout, navExercises, navMyVision, navCommunication]
+const sectionsArray = [sectionHome, sectionAbout, sectionExercises, sectionMyVision, sectionCommunication]
 
 let lastScrollTop = 0;
 
@@ -18,7 +32,13 @@ window.addEventListener('scroll', function() {
 
 
 
-
+// scroll into view events clause 
 moveToExercisesBtn.addEventListener('click', ()=>{
   sectionExercises.scrollIntoView()
 })
+
+for(let i = 0; i < navAnchorsArray.length; i++){
+  navAnchorsArray[i].addEventListener('click', ()=>{
+    sectionsArray[i].scrollIntoView()
+  })
+}
