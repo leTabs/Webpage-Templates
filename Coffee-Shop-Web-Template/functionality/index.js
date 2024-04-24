be338b55b06d6764121c859c421e2590331efd52
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-24 23:10:20 +0300

    Update index.js

--- Coffee-Shop-Web-Template/functionality/index.js
+++ Coffee-Shop-Web-Template/functionality/index.js
@@ -1,12 +1,18 @@
 // nav elements
 const navHome = document.querySelector('.nav-home')
+const navOurCoffee = document.querySelector('.nav-our-coffee')
 
+const navElementsArray = [navHome, navOurCoffee]
 
-const navElementsArray = [navHome]
+// section elements
+const sectionHome = document.querySelector('.section-home')
+const sectionOurCoffee = document.querySelector('.section-our-coffee')
+
+const sectionElementsArray = [sectionHome, sectionOurCoffee]
 
 
 for(let i = 0; i < navElementsArray.length; i++){
     navElementsArray[i].addEventListener('click', ()=>{
-        alert('pop')
+        sectionElementsArray[i].scrollIntoView()
     })
 }
