64afbf2df20c6b18885f401a733ad8de2567645f
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-25 22:19:02 +0300

    Update index.js

--- Coffee-Shop-Web-Template/functionality/index.js
+++ Coffee-Shop-Web-Template/functionality/index.js
@@ -1,14 +1,19 @@
 // nav elements
 const navHome = document.querySelector('.nav-home')
 const navOurCoffee = document.querySelector('.nav-our-coffee')
+const navHistory = document.querySelector('.nav-history')
+const navInfo = document.querySelector('.nav-info')
+const navMenu = document.querySelector('.nav-menu')
 
-const navElementsArray = [navHome, navOurCoffee]
+const navElementsArray = [navHome, navOurCoffee, navHistory, navInfo]
 
 // section elements
 const sectionHome = document.querySelector('.section-home')
 const sectionOurCoffee = document.querySelector('.section-our-coffee')
+const sectionHistory = document.querySelector('.section-history')
+const sectionInfo = document.querySelector('.section-info')
 
-const sectionElementsArray = [sectionHome, sectionOurCoffee]
+const sectionElementsArray = [sectionHome, sectionOurCoffee, sectionHistory, sectionInfo]
 
 
 for(let i = 0; i < navElementsArray.length; i++){
@@ -16,3 +21,7 @@ for(let i = 0; i < navElementsArray.length; i++){
         sectionElementsArray[i].scrollIntoView()
     })
 }
+
+navMenu.addEventListener('click', ()=>{
+    alert('Menu panel')
+})
