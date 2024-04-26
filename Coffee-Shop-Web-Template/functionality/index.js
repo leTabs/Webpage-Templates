0ff40e216bd9bfb3913a1d3e064e7f4284880d74
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-26 18:43:00 +0300

    Update index.js

--- Coffee-Shop-Web-Template/functionality/index.js
+++ Coffee-Shop-Web-Template/functionality/index.js
@@ -15,6 +15,10 @@ const sectionInfo = document.querySelector('.section-info')
 
 const sectionElementsArray = [sectionHome, sectionOurCoffee, sectionHistory, sectionInfo]
 
+// social media elements
+const allInstaIcons = document.querySelectorAll('.insta-icon')
+const allFacebookIcons = document.querySelectorAll('.facebook-icon')
+const allXIcons = document.querySelectorAll('.x-icon')
 // 
 const menuWindowWrapper = document.querySelector('.menu-window-wrapper');
 const menuCloseBtn = document.querySelector('.menu-close-btn')
@@ -41,3 +45,25 @@ document.addEventListener('keydown', function(event){
         document.querySelector('html').style.overflowY = 'auto'
 
 }})
+
+// social media anchors
+allInstaIcons[0].addEventListener('click', ()=>{
+    window.location = 'https://www.instagram.com/'
+})
+allInstaIcons[1].addEventListener('click', ()=>{
+    window.location = 'https://www.instagram.com/'
+})
+
+allFacebookIcons[0].addEventListener('click', ()=>{
+    window.location = 'https://www.facebook.com/'
+})
+allFacebookIcons[1].addEventListener('click', ()=>{
+    window.location = 'https://www.facebook.com/'
+})
+
+allXIcons[0].addEventListener('click', ()=>{
+    window.location = 'https://www.x.com/'
+})
+allXIcons[1].addEventListener('click', ()=>{
+    window.location = 'https://www.x.com/'
+})
